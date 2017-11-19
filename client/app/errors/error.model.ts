c7cd5c0d49a91dfd047a429d6213d60e9afd7be8
Joydeep <joyrcc@gmail.com>
2017-11-19 10:22:47 +0530

    Revert "Initial Code"
    This reverts commit a59eb32f3693554996f60cd7e8358b4cc3767516.

--- client/app/errors/error.model.ts
+++
@@ -1,8 +0,0 @@
-export class Error {
-    /**
-     * Constructor for Error class
-     * @param title
-     * @param message
-     */
-    constructor(public title: string, public message: string) {}
-}
--- mogo.bat
+++
@@ -1,1 +0,0 @@
-start C:\MongoDB\Server\3.4\bin\mongod --dbpath D:\Data1
